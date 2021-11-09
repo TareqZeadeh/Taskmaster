8a7a32bacb0425884d55cd38a0a3835f02c35c75
Tareq Zeadeh <tareq.ha130@yahoo.com>
2021-11-09 18:28:48 +0200

    lab 32 done

--- app/src/main/java/com/example/taskmaster/Add_Task.java
+++ app/src/main/java/com/example/taskmaster/Add_Task.java
@@ -2,17 +2,23 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
+
 public class Add_Task extends AppCompatActivity {
-public static int c =0;
+    private static final String TAG = "Add_Task";
+    public static int c =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +36,11 @@ public static int c =0;
 
                 TextView textView = findViewById(R.id.textView5);
                 c++;
+
+                com.amplifyframework.datastore.generated.model.Task task1 = com.amplifyframework.datastore.generated.model.Task.builder()
+                        .title(title).body(description).state(State.NEW.toString()).build();
+                apiTaskSave(task1);
+
                 textView.setText("Total Tasks :"+c);
                 SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(Add_Task.this);
                 SharedPreferences.Editor editor = preferences.edit();
@@ -37,6 +48,8 @@ public static int c =0;
                 editor.apply();
                 Toast toast = Toast.makeText(getApplicationContext(),"Submitted!", Toast.LENGTH_SHORT);
                 toast.show();
+                Intent intent = new Intent(Add_Task.this,MainActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -48,5 +61,11 @@ public static int c =0;
         int c = Preferences.getInt("c",0);
         TextView textView = findViewById(R.id.textView5);
         textView.setText("Total Tasks :"+c);
+    }
+
+    public void apiTaskSave(com.amplifyframework.datastore.generated.model.Task task){
+        Amplify.API.mutate(ModelMutation.create(task),
+                taskSaved ->{ Log.i(TAG, "Task Is Saved => " + taskSaved.getData().getTitle());},
+                error->{Log.e(TAG, "Task Is Not Saved => " + error.toString());});
     }
 }
