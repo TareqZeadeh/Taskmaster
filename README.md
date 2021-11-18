68a20283585dc9e84c67604ee4ce030dc06e691d
Tareq Zeadeh <tareq.ha130@yahoo.com>
2021-11-18 19:43:49 +0200

    lab-38-done

--- README.md
+++ README.md
@@ -81,11 +81,16 @@
 
 ## ================================================
 
-## lab 36
+## lab 37
 ### S3 Uploads: Adding the allow users to optionally select a file to attach to that task. If a user attaches a file to a task, that file should be uploaded to S3, and associated with that task, On the Task detail activity , the image will appear and a text contains the URL , when pressing on it will direct you the URL page .   
 ### Add Task Page
 ![](lab-37-add-task.png)
 ### Task Detail Page
 ![](lab-37-taskD.png)
+## ================================================
+
+## lab 38
+### Notifications Added.
+![lab-38-notification](lab-38-notification.png)
 
  
--- app/src/main/java/com/example/taskmaster/SignIn.java
+++ app/src/main/java/com/example/taskmaster/SignIn.java
@@ -3,7 +3,10 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -22,10 +25,27 @@ import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 public class SignIn extends AppCompatActivity {
     Handler handler;
 
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(PushListenerService.CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        createNotificationChannel();
         try {
 //            Amplify.addPlugin(new AWSDataStorePlugin());
             Amplify.addPlugin(new AWSApiPlugin());
